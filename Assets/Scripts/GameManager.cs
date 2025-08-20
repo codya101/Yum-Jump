b8ac6752c2721d4fd91d5d4dc6d8996b875682ec
codya <codya100@gmail.com>
2025-08-20 12:38:58 -0400

    Fruit Items

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Player player;
+
+    [Header("Fruits Management")]
+    public bool fruitsHaveRandomLook;
+    public int fruitsCollected;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddFruit() => fruitsCollected++;
+    public bool FruitsHaveRandomLook() => fruitsHaveRandomLook;
+}
