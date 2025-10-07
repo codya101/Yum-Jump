c1c2b04353080f0c0efdd7c4e55bd3cc3fcebeea
codya <codya100@gmail.com>
2025-10-07 11:45:35 -0400

    Saw Trap

---
+++ Assets/Scripts/Trap_Saw.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+public class Trap_Saw : MonoBehaviour
+{
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float cooldown = 1f;
+    [SerializeField] private Transform[] wayPoints;
+    private Vector3[] wayPointPositions;
+
+    public int wayPointIndex = 1;
+    public int moveDirection = 1;
+    private bool canMove = true;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        UpdateWaypointsInfo();
+        transform.position = wayPointPositions[0];
+    }
+
+    private void UpdateWaypointsInfo()
+    {
+        wayPointPositions = new Vector3[wayPoints.Length];
+
+        for (int i = 0; i < wayPoints.Length; i++)
+        {
+            wayPointPositions[i] = wayPoints[i].position;
+        }
+    }
+
+    private void Update()
+    {
+        anim.SetBool("active", canMove);
+
+        if (canMove == false)
+            return;
+
+        transform.position = Vector2.MoveTowards(transform.position, wayPointPositions[wayPointIndex], moveSpeed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, wayPointPositions[wayPointIndex]) < 0.1f)
+        {
+            if (wayPointIndex == wayPointPositions.Length - 1 || wayPointIndex == 0)
+            {
+                moveDirection *= -1;
+                StartCoroutine(StopMovement(cooldown));
+            }
+
+            wayPointIndex += moveDirection;
+        }
+    }
+
+    private IEnumerator StopMovement(float delay)
+    {
+        canMove = false;
+
+        yield return new WaitForSeconds(delay);
+
+        canMove = true;
+        sr.flipX = !sr.flipX;
+    }
+}
